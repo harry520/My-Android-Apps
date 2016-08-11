8106a30704aebe5f7d683914b223511406092fe7
Tenghan Jiang <harryjiang_520@hotmail.com>
2016-08-10 23:04:21 -0700

    Sound Recording Demo

---
+++ SoundRecordingDemo/src/com/example/soundrecordingdemo/AppLog.java
@@ -0,0 +1,13 @@
+package com.example.soundrecordingdemo;
+
+import android.util.Log;
+
+public class AppLog {
+
+	private static final String APP_TAG = "AudioRecorder";
+
+	public static int logString(String message) {
+		return Log.i(APP_TAG, message);
+	}
+
+}
