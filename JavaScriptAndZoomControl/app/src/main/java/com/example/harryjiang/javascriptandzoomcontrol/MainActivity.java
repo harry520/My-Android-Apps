de25de565710e5b0bfb599d490d72504b1324e5d
Tenghan Jiang <harryjiang_520@hotmail.com>
2016-11-28 15:07:59 -0800

    JavaScript and Zoom Control

---
+++ JavaScriptAndZoomControl/app/src/main/java/com/example/harryjiang/javascriptandzoomcontrol/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.harryjiang.javascriptandzoomcontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+    final String DEFAULT_URL = "https://developer.android.com/index.html";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.setWebViewClient(new MyWebViewClient());
+        webView.loadUrl(DEFAULT_URL);
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+}
